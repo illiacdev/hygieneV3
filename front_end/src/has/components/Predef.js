ba7e39fd1c112bf66f35ecd2293b00350dabd671
illiacdev <illiacdev@gmail.com>
2023-03-06 11:33:37 +0900

    세일병병

--- front_end/src/has/components/Predef.js
+++ front_end/src/has/components/Predef.js
@@ -1,7 +1,7 @@
 export class Predef {
     // static _병원병 = "성빈센트병원";
-    // static _병원병 = "세일병원";
-    static _병원병 = "인제대학교서울백병원";
+    static _병원병 = "세일병원";
+    // static _병원병 = "인제대학교서울백병원";
 
     // static _병원템플릿 = "성빈센트";
     // static _병원템플릿 = "세일병원";
